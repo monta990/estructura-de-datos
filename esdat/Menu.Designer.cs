8675902cffb19df95571d0814bdfcf81521a1fee
Edwin Elias Alvarez <Edwin Elias Alvarez>
2017-09-05 17:01:28 -0700

    Add project files.

---
+++ esdat/Menu.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace esdat
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.unidad1ConseptosBásicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad2ArreglosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad3ListasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad4PilasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad5ColasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad6ArbolesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tiposDeDatosAbstractosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recursividadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.practicaUnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fibonnachiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mCDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.busquedaBinariaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pruebaDeFibonnachiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fractalDeHilbertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unidad1ConseptosBásicosToolStripMenuItem,
+            this.unidad2ArreglosToolStripMenuItem,
+            this.unidad3ListasToolStripMenuItem,
+            this.unidad4PilasToolStripMenuItem,
+            this.unidad5ColasToolStripMenuItem,
+            this.unidad6ArbolesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(711, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // unidad1ConseptosBásicosToolStripMenuItem
+            // 
+            this.unidad1ConseptosBásicosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiposDeDatosAbstractosToolStripMenuItem,
+            this.recursividadToolStripMenuItem});
+            this.unidad1ConseptosBásicosToolStripMenuItem.Name = "unidad1ConseptosBásicosToolStripMenuItem";
+            this.unidad1ConseptosBásicosToolStripMenuItem.Size = new System.Drawing.Size(170, 20);
+            this.unidad1ConseptosBásicosToolStripMenuItem.Text = "Unidad 1: Conseptos Básicos";
+            // 
+            // unidad2ArreglosToolStripMenuItem
+            // 
+            this.unidad2ArreglosToolStripMenuItem.Name = "unidad2ArreglosToolStripMenuItem";
+            this.unidad2ArreglosToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.unidad2ArreglosToolStripMenuItem.Text = "Unidad 2: Arreglos";
+            this.unidad2ArreglosToolStripMenuItem.Click += new System.EventHandler(this.unidad2ArreglosToolStripMenuItem_Click);
+            // 
+            // unidad3ListasToolStripMenuItem
+            // 
+            this.unidad3ListasToolStripMenuItem.Name = "unidad3ListasToolStripMenuItem";
+            this.unidad3ListasToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.unidad3ListasToolStripMenuItem.Text = "Unidad 3: Listas";
+            // 
+            // unidad4PilasToolStripMenuItem
+            // 
+            this.unidad4PilasToolStripMenuItem.Name = "unidad4PilasToolStripMenuItem";
+            this.unidad4PilasToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.unidad4PilasToolStripMenuItem.Text = "Unidad 4: Pilas";
+            // 
+            // unidad5ColasToolStripMenuItem
+            // 
+            this.unidad5ColasToolStripMenuItem.Name = "unidad5ColasToolStripMenuItem";
+            this.unidad5ColasToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.unidad5ColasToolStripMenuItem.Text = "Unidad 5: Colas";
+            // 
+            // unidad6ArbolesToolStripMenuItem
+            // 
+            this.unidad6ArbolesToolStripMenuItem.Name = "unidad6ArbolesToolStripMenuItem";
+            this.unidad6ArbolesToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.unidad6ArbolesToolStripMenuItem.Text = "Unidad 6: Arboles";
+            // 
+            // tiposDeDatosAbstractosToolStripMenuItem
+            // 
+            this.tiposDeDatosAbstractosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.practicaUnoToolStripMenuItem});
+            this.tiposDeDatosAbstractosToolStripMenuItem.Name = "tiposDeDatosAbstractosToolStripMenuItem";
+            this.tiposDeDatosAbstractosToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.tiposDeDatosAbstractosToolStripMenuItem.Text = "Tipos de datos abstractos";
+            // 
+            // recursividadToolStripMenuItem
+            // 
+            this.recursividadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fibonnachiToolStripMenuItem,
+            this.mCDToolStripMenuItem,
+            this.busquedaBinariaToolStripMenuItem,
+            this.pruebaDeFibonnachiToolStripMenuItem,
+            this.fractalDeHilbertToolStripMenuItem});
+            this.recursividadToolStripMenuItem.Name = "recursividadToolStripMenuItem";
+            this.recursividadToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+            this.recursividadToolStripMenuItem.Text = "Recursividad";
+            // 
+            // practicaUnoToolStripMenuItem
+            // 
+            this.practicaUnoToolStripMenuItem.Name = "practicaUnoToolStripMenuItem";
+            this.practicaUnoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.practicaUnoToolStripMenuItem.Text = "Practica Uno";
+            // 
+            // fibonnachiToolStripMenuItem
+            // 
+            this.fibonnachiToolStripMenuItem.Name = "fibonnachiToolStripMenuItem";
+            this.fibonnachiToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.fibonnachiToolStripMenuItem.Text = "Fibonnachi";
+            // 
+            // mCDToolStripMenuItem
+            // 
+            this.mCDToolStripMenuItem.Name = "mCDToolStripMenuItem";
+            this.mCDToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.mCDToolStripMenuItem.Text = "MCD";
+            // 
+            // busquedaBinariaToolStripMenuItem
+            // 
+            this.busquedaBinariaToolStripMenuItem.Name = "busquedaBinariaToolStripMenuItem";
+            this.busquedaBinariaToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.busquedaBinariaToolStripMenuItem.Text = "Busqueda Binaria";
+            // 
+            // pruebaDeFibonnachiToolStripMenuItem
+            // 
+            this.pruebaDeFibonnachiToolStripMenuItem.Name = "pruebaDeFibonnachiToolStripMenuItem";
+            this.pruebaDeFibonnachiToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.pruebaDeFibonnachiToolStripMenuItem.Text = "Prueba de Fibonnachi";
+            // 
+            // fractalDeHilbertToolStripMenuItem
+            // 
+            this.fractalDeHilbertToolStripMenuItem.Name = "fractalDeHilbertToolStripMenuItem";
+            this.fractalDeHilbertToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.fractalDeHilbertToolStripMenuItem.Text = "Fractal de Hilbert";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 270);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Memú: Estructura de Datos";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem unidad1ConseptosBásicosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad2ArreglosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad3ListasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad4PilasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tiposDeDatosAbstractosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem practicaUnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recursividadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fibonnachiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mCDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem busquedaBinariaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pruebaDeFibonnachiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fractalDeHilbertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad5ColasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad6ArbolesToolStripMenuItem;
+    }
+}
+
---
+++ esdat/TipoDatos.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace esdat
+{
+    partial class TipoDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.rBint = new System.Windows.Forms.RadioButton();
+            this.rBdouble = new System.Windows.Forms.RadioButton();
+            this.rBdecimal = new System.Windows.Forms.RadioButton();
+            this.rBchar = new System.Windows.Forms.RadioButton();
+            this.rBstring = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rBstring);
+            this.groupBox1.Controls.Add(this.rBchar);
+            this.groupBox1.Controls.Add(this.rBdecimal);
+            this.groupBox1.Controls.Add(this.rBdouble);
+            this.groupBox1.Controls.Add(this.rBint);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 194);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos soportados";
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.Location = new System.Drawing.Point(197, 226);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuar.TabIndex = 1;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = true;
+            // 
+            // rBint
+            // 
+            this.rBint.AutoSize = true;
+            this.rBint.Location = new System.Drawing.Point(18, 31);
+            this.rBint.Name = "rBint";
+            this.rBint.Size = new System.Drawing.Size(43, 17);
+            this.rBint.TabIndex = 0;
+            this.rBint.TabStop = true;
+            this.rBint.Text = "INT";
+            this.rBint.UseVisualStyleBackColor = true;
+            // 
+            // rBdouble
+            // 
+            this.rBdouble.AutoSize = true;
+            this.rBdouble.Location = new System.Drawing.Point(18, 54);
+            this.rBdouble.Name = "rBdouble";
+            this.rBdouble.Size = new System.Drawing.Size(69, 17);
+            this.rBdouble.TabIndex = 1;
+            this.rBdouble.TabStop = true;
+            this.rBdouble.Text = "DOUBLE";
+            this.rBdouble.UseVisualStyleBackColor = true;
+            // 
+            // rBdecimal
+            // 
+            this.rBdecimal.AutoSize = true;
+            this.rBdecimal.Location = new System.Drawing.Point(18, 77);
+            this.rBdecimal.Name = "rBdecimal";
+            this.rBdecimal.Size = new System.Drawing.Size(72, 17);
+            this.rBdecimal.TabIndex = 2;
+            this.rBdecimal.TabStop = true;
+            this.rBdecimal.Text = "DECIMAL";
+            this.rBdecimal.UseVisualStyleBackColor = true;
+            // 
+            // rBchar
+            // 
+            this.rBchar.AutoSize = true;
+            this.rBchar.Location = new System.Drawing.Point(18, 100);
+            this.rBchar.Name = "rBchar";
+            this.rBchar.Size = new System.Drawing.Size(55, 17);
+            this.rBchar.TabIndex = 3;
+            this.rBchar.TabStop = true;
+            this.rBchar.Text = "CHAR";
+            this.rBchar.UseVisualStyleBackColor = true;
+            // 
+            // rBstring
+            // 
+            this.rBstring.AutoSize = true;
+            this.rBstring.Location = new System.Drawing.Point(18, 123);
+            this.rBstring.Name = "rBstring";
+            this.rBstring.Size = new System.Drawing.Size(66, 17);
+            this.rBstring.TabIndex = 4;
+            this.rBstring.TabStop = true;
+            this.rBstring.Text = "STRING";
+            this.rBstring.UseVisualStyleBackColor = true;
+            // 
+            // TipoDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnContinuar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TipoDatos";
+            this.Text = "Selecciona el tipo de datos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.RadioButton rBstring;
+        private System.Windows.Forms.RadioButton rBchar;
+        private System.Windows.Forms.RadioButton rBdecimal;
+        private System.Windows.Forms.RadioButton rBdouble;
+        private System.Windows.Forms.RadioButton rBint;
+    }
+}

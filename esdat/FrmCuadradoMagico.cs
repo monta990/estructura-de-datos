1fb7a63fb981b64975da20dd2c0bca748e478001
Edwin Elias Alvarez <elias986@yahoo.com>
2017-10-05 03:19:19 -0700

    Validación de cuadromagico no vacio

--- esdat/FrmCuadradoMagico.cs
+++ esdat/FrmCuadradoMagico.cs
@@ -73,42 +73,51 @@ namespace esdat
             lbDiagonal2.Text = "0";
         }
         /// <summary>
-        /// Calculo de diagonales, renglones y columnas del cuadrado magico, no recibe parametros
+        /// Valida el datagridview antes calcular
         /// </summary>
-        private void Calcular() //calcular diagoanles
+        private void Validar() //calcular diagoanles
         {
             for (int ren = 0, cel = 0; ren < 4; ren++)
             {
-                if ((String)dGVcuadradoMagico.Rows[ren].Cells[cel].Value == null )
+                if ((String)dGVcuadradoMagico.Rows[ren].Cells[cel].Value == null)
                 {
                     MessageBox.Show("Alguna celda esta vacia", "Celda Vacia", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
+                    break;
                 }
                 else
                 {
-                    int[] suma = new int[10];
-                    for (int i = 0, r = 3; i < 4; i++, r--) //todo en uno de una vez
-                    {
-                        lbDiagonal1.Text = (suma[0] += int.Parse(dGVcuadradoMagico[i, i].Value.ToString())).ToString();
-                        lbDiagonal2.Text = (suma[1] += int.Parse(dGVcuadradoMagico[i, r].Value.ToString())).ToString();
-                        lbRenglon1.Text = (suma[2] += int.Parse(dGVcuadradoMagico[i, 0].Value.ToString())).ToString();
-                        lbRenglon2.Text = (suma[3] += int.Parse(dGVcuadradoMagico[i, 1].Value.ToString())).ToString();
-                        lbRenglon3.Text = (suma[4] += int.Parse(dGVcuadradoMagico[i, 2].Value.ToString())).ToString();
-                        lbRenglon4.Text = (suma[5] += int.Parse(dGVcuadradoMagico[i, 3].Value.ToString())).ToString();
-                        lbColumna1.Text = (suma[6] += int.Parse(dGVcuadradoMagico[0, r].Value.ToString())).ToString();
-                        lbColumna2.Text = (suma[7] += int.Parse(dGVcuadradoMagico[1, r].Value.ToString())).ToString();
-                        lbColumna3.Text = (suma[8] += int.Parse(dGVcuadradoMagico[2, r].Value.ToString())).ToString();
-                        lbColumna4.Text = (suma[9] += int.Parse(dGVcuadradoMagico[3, r].Value.ToString())).ToString();
-                    }
-                    if (suma[9] == suma[8] && suma[8] == suma[7] && suma[7] == suma[6] && suma[6] == suma[5] && suma[5] == suma[4] && suma[4] == suma[3] && suma[3] == suma[2] && suma[2] == suma[1])
-                    {
-                        MessageBox.Show("Cuadrado magico", "Si es cuadrado magico", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    else
-                    {
-                        MessageBox.Show("No es", "No es cuadrado magico", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
+                    Calcular();
+                    break;
                 }
+            }
+            
+        }
+        /// <summary>
+        /// Calculo de diagonales, renglones y columnas del cuadrado magico, no recibe parametros
+        /// </summary>
+        private void Calcular()
+        {
+            int[] suma = new int[10];
+            for (int i = 0, r = 3; i < 4; i++, r--) //todo en uno de una vez
+            {
+                lbDiagonal1.Text = (suma[0] += int.Parse(dGVcuadradoMagico[i, i].Value.ToString())).ToString();
+                lbDiagonal2.Text = (suma[1] += int.Parse(dGVcuadradoMagico[i, r].Value.ToString())).ToString();
+                lbRenglon1.Text = (suma[2] += int.Parse(dGVcuadradoMagico[i, 0].Value.ToString())).ToString();
+                lbRenglon2.Text = (suma[3] += int.Parse(dGVcuadradoMagico[i, 1].Value.ToString())).ToString();
+                lbRenglon3.Text = (suma[4] += int.Parse(dGVcuadradoMagico[i, 2].Value.ToString())).ToString();
+                lbRenglon4.Text = (suma[5] += int.Parse(dGVcuadradoMagico[i, 3].Value.ToString())).ToString();
+                lbColumna1.Text = (suma[6] += int.Parse(dGVcuadradoMagico[0, r].Value.ToString())).ToString();
+                lbColumna2.Text = (suma[7] += int.Parse(dGVcuadradoMagico[1, r].Value.ToString())).ToString();
+                lbColumna3.Text = (suma[8] += int.Parse(dGVcuadradoMagico[2, r].Value.ToString())).ToString();
+                lbColumna4.Text = (suma[9] += int.Parse(dGVcuadradoMagico[3, r].Value.ToString())).ToString();
+            }
+            if (suma[9] == suma[8] && suma[8] == suma[7] && suma[7] == suma[6] && suma[6] == suma[5] && suma[5] == suma[4] && suma[4] == suma[3] && suma[3] == suma[2] && suma[2] == suma[1])
+            {
+                MessageBox.Show("Si es cuadrado magico", "Hay magia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No es cuadrado magico", "Sin magia", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         private void FrmCuadradoMagico_Load(object sender, EventArgs e)
@@ -131,13 +140,12 @@ namespace esdat
         }
         private void btCalcular_Click(object sender, EventArgs e)
         {
-            Calcular();
+            Validar();
 
         }
         private void dGVcuadradoMagico_CellLeave(object sender, DataGridViewCellEventArgs e)
         {
-            //int.TryParse(dGVcuadradoMagico.CurrentCell.Value.ToString(), out int res);
-            //MessageBox.Show(dGVcuadradoMagico.CurrentCell.ColumnIndex + " " + dGVcuadradoMagico.CurrentCell.RowIndex);
+            
         }
     }
 }

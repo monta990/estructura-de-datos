7f395cb60a19ab15874a0318b3e7b9c1f196d6eb
Edwin Elias Alvarez <elias986@yahoo.com>
2017-12-05 17:11:28 -0700

    Corrección de fallo al volver a validar un expresión erronea, el cual siempre devolvia fallido, aunque no fuera.

--- esdat/FrmPosfijas.cs
+++ esdat/FrmPosfijas.cs
@@ -20,136 +20,25 @@ namespace esdat
                 dGVposfija.Rows.Add(item.ToString());
             }
         }
+        /// <summary>
+        /// Limpia los campos y pila del form
+        /// </summary>
         private void Limpiar()
         {
             lblResultado.Text = "0";
             dGVposfija.Rows.Clear();
             tbExpresion.Clear();
             pilaint.Clear();
+            truena = false;
         }
-        private void btmPasoaPaso_Click(object sender, EventArgs e)
-        {
-            lblResultado.Text = "0";
-            pilaint.Clear();
-            dGVposfija.Rows.Clear();
-            string[] expresion = tbExpresion.Text.Split(' ');
-            for (int i = 0; i < expresion.Length; i++)
-            {
-                try
-                {
-                    int operando = int.Parse(expresion[i]);
-                    pilaint.Push(operando);
-                    MessageBox.Show(expresion[i] + " es un operando, va a la pila", "Es operando", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                catch (Exception deter)
-                {
-                    switch (expresion[i])
-                    {
-                        case "+":
-                            if (pilaint.Count >= 2)
-                            {
-                                op1 = pilaint.Pop();
-                                op2 = pilaint.Pop();
-                                int suma = op2 + op1;
-                                MessageBox.Show("Push(" + op2 + " + " + op1 + ")", "Suma", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                pilaint.Push(suma);
-                                MessageBox.Show("Por lo cual: " + suma + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }
-                            else
-                            {
-                                truena = true;                
-                            }
-                            break;
-                        case "-":
-                            if (pilaint.Count >= 2)
-                            {
-                                op1 = pilaint.Pop();
-                                op2 = pilaint.Pop();
-                                int resta = op2 - op1;
-                                MessageBox.Show("Push(" + op2 + " - " + op1 + ")", "Resta", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                pilaint.Push(resta);
-                                MessageBox.Show("Por lo cual: " + resta + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }
-                            else
-                            {
-                                truena = true;
-                            }
-                            break;
-                        case "*":
-                            if (pilaint.Count == 2)
-                            {
-                                op1 = pilaint.Pop();
-                                op2 = pilaint.Pop();
-                                int multi = op2 * op1;
-                                MessageBox.Show("Push(" + op2 + " * " + op1 + ")", "Multiplicación", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                pilaint.Push(multi);
-                                MessageBox.Show("Por lo cual: " + multi + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }
-                            else
-                            {
-                                truena = true;
-                            }
-                            break;
-                        case "/":
-                            
-                            if (pilaint.Count == 2)
-                            {
-                                op1 = pilaint.Pop();
-                                op2 = pilaint.Pop();
-                                int dividir = op2 / op1;
-                                MessageBox.Show("Push(" + op2 + " / " + op1 + ")", "Divición", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                pilaint.Push(dividir);
-                                MessageBox.Show("Por lo cual: " + dividir + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }
-                            else
-                            {
-                                truena = true;
-                            }
-                            break;
-                        default:
-                            MessageBox.Show("No fue operando ni operarador, caracter incorrecto","Caracter no valido", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            break;
-                    }
-                }
-                Imprimirpila();
-                if (truena) //salir del for en caso de fallo
-                break;
-            }
-            if (truena == true)
-            {
-                lblResultado.Text = "";
-                MessageBox.Show("Error en la expresión se termino con un operando", "Expresión incorrecta", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                if (pilaint.Count == 1)
-                {
-                    MessageBox.Show("El resultado es: " + pilaint.Peek(), "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    lblResultado.Text = pilaint.Peek().ToString();
-                }
-                else
-                {
-                    lblResultado.Text = "";
-                    MessageBox.Show("Error en la expresión", "Expresión incorrecta", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-        private void btnEjemplo1_Click(object sender, EventArgs e)
-        {
-            Limpiar();
-            tbExpresion.Text = "1 2 - 3 * 4 / 5 + 6 +";
-        }
-        private void btnEjemplo2_Click(object sender, EventArgs e)
-        {
-            Limpiar();
-            tbExpresion.Text = "1 2 - 3 * 4 / 5 + 6 + 7 * 8 + 9 +";
-        }
-        private void btnSalir_Click(object sender, EventArgs e) => this.Close();
-        private void btnReinicar_Click(object sender, EventArgs e) => Limpiar();
-        private void btnEvaluar_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Ejecuta la operación
+        /// </summary>
+        private void Evaluar()
         {
             pilaint.Clear();
             dGVposfija.Rows.Clear();
+            truena = false;
             string[] expresion = tbExpresion.Text.Split(' ');
             for (int i = 0; i < expresion.Length; i++)
             {
@@ -243,5 +132,130 @@ namespace esdat
                 }
             }
         }
+        /// <summary>
+        /// Ejecuta la operación paso a paso y lo muestra en pantalla
+        /// </summary>
+        private void Pasoapaso()
+        {
+            lblResultado.Text = "0";
+            pilaint.Clear();
+            dGVposfija.Rows.Clear();
+            truena = false;
+            string[] expresion = tbExpresion.Text.Split(' ');
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                try
+                {
+                    int operando = int.Parse(expresion[i]);
+                    pilaint.Push(operando);
+                    MessageBox.Show(expresion[i] + " es un operando, va a la pila", "Es operando", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception deter)
+                {
+                    switch (expresion[i])
+                    {
+                        case "+":
+                            if (pilaint.Count >= 2)
+                            {
+                                op1 = pilaint.Pop();
+                                op2 = pilaint.Pop();
+                                int suma = op2 + op1;
+                                MessageBox.Show("Push(" + op2 + " + " + op1 + ")", "Suma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                pilaint.Push(suma);
+                                MessageBox.Show("Por lo cual: " + suma + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                truena = true;
+                            }
+                            break;
+                        case "-":
+                            if (pilaint.Count >= 2)
+                            {
+                                op1 = pilaint.Pop();
+                                op2 = pilaint.Pop();
+                                int resta = op2 - op1;
+                                MessageBox.Show("Push(" + op2 + " - " + op1 + ")", "Resta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                pilaint.Push(resta);
+                                MessageBox.Show("Por lo cual: " + resta + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                truena = true;
+                            }
+                            break;
+                        case "*":
+                            if (pilaint.Count == 2)
+                            {
+                                op1 = pilaint.Pop();
+                                op2 = pilaint.Pop();
+                                int multi = op2 * op1;
+                                MessageBox.Show("Push(" + op2 + " * " + op1 + ")", "Multiplicación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                pilaint.Push(multi);
+                                MessageBox.Show("Por lo cual: " + multi + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                truena = true;
+                            }
+                            break;
+                        case "/":
+
+                            if (pilaint.Count == 2)
+                            {
+                                op1 = pilaint.Pop();
+                                op2 = pilaint.Pop();
+                                int dividir = op2 / op1;
+                                MessageBox.Show("Push(" + op2 + " / " + op1 + ")", "Divición", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                pilaint.Push(dividir);
+                                MessageBox.Show("Por lo cual: " + dividir + " va a la pila", "A la Pila", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                truena = true;
+                            }
+                            break;
+                        default:
+                            MessageBox.Show("No fue operando ni operarador, caracter incorrecto", "Caracter no valido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+                }
+                Imprimirpila();
+                if (truena) //salir del for en caso de fallo
+                    break;
+            }
+            if (truena == true)
+            {
+                lblResultado.Text = "";
+                MessageBox.Show("Error en la expresión se termino con un operando", "Expresión incorrecta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (pilaint.Count == 1)
+                {
+                    MessageBox.Show("El resultado es: " + pilaint.Peek(), "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lblResultado.Text = pilaint.Peek().ToString();
+                }
+                else
+                {
+                    lblResultado.Text = "";
+                    MessageBox.Show("Error en la expresión", "Expresión incorrecta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void btmPasoaPaso_Click(object sender, EventArgs e) => Pasoapaso();
+        private void btnEjemplo1_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            tbExpresion.Text = "1 2 - 3 * 4 / 5 + 6 +";
+        }
+        private void btnEjemplo2_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            tbExpresion.Text = "1 2 - 3 * 4 / 5 + 6 + 7 * 8 + 9 +";
+        }
+        private void btnSalir_Click(object sender, EventArgs e) => this.Close();
+        private void btnReinicar_Click(object sender, EventArgs e) => Limpiar();
+        private void btnEvaluar_Click(object sender, EventArgs e) => Evaluar();
     }
 }
